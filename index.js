c40043463de3aa2b51759735530d6350df543902
Rowney Ouma <oumarowney@gmail.com>
2025-04-21 18:17:43 +0300

    task complete

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+	
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });
 }
